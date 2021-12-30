5a885170a87b90baaed22f095b11b6e9471a467d
John <souzajohnlee@gmail.com>
2021-12-30 11:19:24 -0800

    Version 1.0.1 - Only direct traffic to production web nodes

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,13 +11,28 @@ haproxy_frontend 'http-in' do
   default_backend 'servers'
 end
 
+# haproxy_backend 'servers' do
+#   server [
+#   	'web1 3.144.181.179:80 maxconn 32',
+#   	'web2 13.58.2.94:80 maxconn 32'
+#   ]
+# #   notifies :reload, 'haproxy_service[haproxy]', :immediately 
+# #   Does not work because of current github issue
+# end
+
+# all_web_nodes = search('node', 'role:web')
+
+production_nodes = search('node', "chef_environment:#{node.chef_environment} AND role:web")
+
+servers = []
+
+production_nodes.each do |node|
+  server = "#{node['name']} #{node['ec2']['public_ipv4']}:80 maxconn 32"
+  servers.push(server)
+end
+
 haproxy_backend 'servers' do
-  server [
-  	'web1 3.144.181.179:80 maxconn 32',
-  	'web2 13.58.2.94:80 maxconn 32'
-  ]
-#   notifies :reload, 'haproxy_service[haproxy]', :immediately 
-#   Does not work because of current github issue
+  server servers
 end
 
 haproxy_service 'haproxy' do
